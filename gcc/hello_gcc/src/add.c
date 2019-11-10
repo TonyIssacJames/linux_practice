9b6fea47a2778e18d807f289142cd7dfe9136821
Tony James <tonyissacjames@gmail.com>
2019-11-11 03:06:05 +0530

    addding the earlier excercises

---
+++ gcc/hello_gcc/src/add.c
@@ -0,0 +1,6 @@
+#include "add.h"
+
+int add_num(int ip1, int ip2)
+{
+	return  (ip1 + ip2);
+}
---
+++ gcc/hello_gcc/test/test_main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "add.h"
+
+
+void main()
+{
+
+	printf("sum of  %d, %d is %d\n", 1, 11, add(1, 11));
+}
